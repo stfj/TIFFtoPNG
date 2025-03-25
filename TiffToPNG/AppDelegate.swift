0587b4e12f82b8eb7869dbbdf5d02805fb2c0f1a
Zach Gage <zach@gage.nyc>
2025-03-25 10:25:39 -0400

    basics

--- TiffToPNG/AppDelegate.swift
+++ TiffToPNG/AppDelegate.swift
@@ -1,30 +1,100 @@
-//
-//  AppDelegate.swift
-//  TiffToPNG
-//
-//  Created by Zach Gage on 3/24/25.
-//
-
 import Cocoa
+import UserNotifications
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet var window: NSWindow!
-
+    var statusItem: NSStatusItem?
+    var timer: Timer?
+    var lastChangeCount = NSPasteboard.general.changeCount
+    var isConversionEnabled = true  // Toggle flag
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        // Create the status item for the menubar
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem?.button {
+            button.title = "Tiff2PNG" // You can also set an image here
+        }
+        
+        // Build the menu for the status item
+        let menu = NSMenu()
+        
+        // Toggle conversion menu item with checkmark state
+        let toggleItem = NSMenuItem(title: "Auto Convert", action: #selector(toggleConversion(_:)), keyEquivalent: "")
+        toggleItem.state = isConversionEnabled ? .on : .off
+        menu.addItem(toggleItem)
+        
+        // Separator for clarity
+        menu.addItem(NSMenuItem.separator())
+        
+        // Quit menu item
+        let quitItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q")
+        menu.addItem(quitItem)
+        
+        statusItem?.menu = menu
+        
+        // Request notification permissions
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
+            if let error = error {
+                print("Notification permission error: \(error)")
+            }
+        }
+        
+        // Start monitoring the clipboard using a timer
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(checkClipboard),
+                                     userInfo: nil,
+                                     repeats: true)
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    @objc func toggleConversion(_ sender: NSMenuItem) {
+        isConversionEnabled.toggle()
+        sender.state = isConversionEnabled ? .on : .off
     }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
+    
+    @objc func quitApp() {
+        NSApp.terminate(nil)
     }
+    
+    @objc func checkClipboard() {
+        let pasteboard = NSPasteboard.general
+        
+        // Only proceed if there is a change in the pasteboard
+        if pasteboard.changeCount != lastChangeCount {
+            lastChangeCount = pasteboard.changeCount
+            
+            // Check for TIFF data and if conversion is enabled
+            if isConversionEnabled,
+               let types = pasteboard.types,
+               types.contains(.tiff),
+               let tiffData = pasteboard.data(forType: .tiff),
+               let image = NSImage(data: tiffData),
+               let pngData = image.pngDataRepresentation() {
+                
+                // Replace the clipboard content with PNG data
+                pasteboard.clearContents()
+                pasteboard.setData(pngData, forType: .png)
+                print("Replaced TIFF with PNG.")
+                
+                // Schedule a system notification
+                let content = UNMutableNotificationContent()
+                content.title = "Clipboard Updated"
+                content.body = "Replaced TIFF with PNG."
+                content.sound = UNNotificationSound.default
 
-
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+            }
+        }
+    }
 }
 
+extension NSImage {
+    /// Returns the PNG data representation of the NSImage.
+    func pngDataRepresentation() -> Data? {
+        guard let tiffData = self.tiffRepresentation,
+              let bitmap = NSBitmapImageRep(data: tiffData) else { return nil }
+        return bitmap.representation(using: .png, properties: [:])
+    }
+}
